9fd73bc988a8380411cf7d433ea0531c18a94185
UmidaImerova <93429355+UmidaImerova@users.noreply.github.com>
2021-12-20 00:07:57 +0600

    main files added

---
+++ data.js
@@ -0,0 +1,34 @@
+export const getData = function() {
+    return [
+        {
+            strName: "lagman",
+            srtСategory: "second course",
+            srtImg: "https://www.vkusnyblog.ru/wp-content/uploads/2014/07/lagman.jpg",
+            amountOfIngredients: "for 6 servings",
+            strIngredient1: "tomato",
+            strIngredient2: "spices",
+            strIngredient3: "onion",
+            strIngredient4: "garlic",
+            strIngredient5: "meat",
+            strIngredient6: "eggplant",
+            strIngredient7: "pepper",
+            strIngredient8: "water",
+            strIngredient9: "egg",
+            strIngredient10: "flour",
+            strMeasure1: "3 pieces",
+            strMeasure2: "1 soup spoon",
+            strMeasure3: "2 pieces",
+            strMeasure4: "4 tine",
+            strMeasure5: "600 grams",
+            strMeasure6: "1 pieces",
+            strMeasure7: "4 pieces",
+            strMeasure8: "1 liter",
+            strMeasure9: "3 pieces",
+            strMeasure10: "450 grams",
+            strInstructions: "https://www.vkusnyblog.ru/recipe/lagman/",
+            strVideo: "https://www.youtube.com/watch?v=dFGZs96aJEw&t=1s"
+        }
+
+    ]
+    
+}
